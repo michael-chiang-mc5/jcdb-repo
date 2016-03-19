75d8ee223dcf02c3050e608a62e49b0394e96958
Michael Chiang <michaelchiang@Michaels-iMac.local>
2016-03-19 03:46:18 -0700

    Note form now saves into db

--- mySite/pdfjs/models.py
+++ mySite/pdfjs/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from Uploader.models import Document
 
-# Create your models here.
+class Note(models.Model):
+    user = models.ForeignKey(User)
+    time = models.DateTimeField(auto_now_add=True)
+    document = models.ForeignKey(Document)
+    page_number = models.PositiveIntegerField()
+    x_normalized_position = models.FloatField()
+    y_normalized_position = models.FloatField()
+    width = models.FloatField()
+    height = models.FloatField()
+
+    def __str__(self):
+        return str(self.page_number)
+    def addText(self,user,text):
+        self.save()
+        noteText = NoteText(user=user,text=text,note=self)
+        noteText.save()
+
+
+class NoteText(models.Model):
+    user = models.ForeignKey(User)
+    time = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    note = models.ForeignKey(Note)
+    def __str__(self):
+        return self.text
--- mySite/pdfjs/views.py
+++ mySite/pdfjs/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from Uploader.views import Document
+from Uploader.models import Document
 from django.conf import settings
 from django.http import JsonResponse
+from .models import *
 
 def index(request):
-    context = {}
+    notes = Note.objects.all()
+    noteTexts = NoteText.objects.all()
+    context = {'notes':notes,'noteTexts':noteTexts}
     return render(request, 'pdfjs/index.html', context)
 
 def viewer(request,document_pk):
@@ -22,7 +25,22 @@ def addNote(request):
     if request.method == 'POST':
         document_pk = request.POST.get("document_pk")
         form_text = request.POST.get("form_text")
-        user = request.user.username
+        user = request.user
+        page_number = request.POST.get("page_number")
+        x_normalized = request.POST.get("x_normalized")
+        y_normalized = request.POST.get("y_normalized")
+        width = request.POST.get("width")
+        height = request.POST.get("height")
+
+        note = Note(user=user,
+                    document=Document.objects.get(pk=document_pk),
+                    page_number=page_number,
+                    x_normalized_position=x_normalized,
+                    y_normalized_position=y_normalized,
+                    width=width,
+                    height=height,)
+        note.save()
+        note.addText(user=user,text=form_text)
         response = {'document_pk':document_pk,}
         return JsonResponse(response)
     else:
