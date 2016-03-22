1274a1452bf3a7033f87dfe7f97224e8a18c54dd
Michael Chiang <michaelchiang@Michaels-iMac.local>
2016-03-22 00:32:07 -0700

    changed mcatdb to jcdb

--- mySite/syndb.sh
+++ mySite/syndb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #!/bin/bash
 
-scp michaelc@104.131.145.75:/home/michaelc/dev/mcatdb-repo/mySite/db.sqlite3 .
-scp -r michaelc@104.131.145.75:/home/michaelc/dev/mcatdb-repo/mySite/media .
+scp michaelc@104.131.145.75:/home/michaelc/dev/jcdb-repo/mySite/db.sqlite3 .
+scp -r michaelc@104.131.145.75:/home/michaelc/dev/jcdb-repo/mySite/media .
