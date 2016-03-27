42624f0fb717895b321328e3ce8953979b32de51
Michael Chiang <michaelchiang@Michaels-iMac.local>
2016-03-26 17:22:38 -0700

    Added permissions checks to Groups/views.py

--- mySite/Groups/views.py
+++ mySite/Groups/views.py
@@ -8,6 +8,7 @@ from UserProfiles.models import *
 from django.conf import settings
 import os
 
+# If user is logged in, show group manager. Otherwise, show landing page
 def index(request):
     if request.user.is_authenticated():
         user = request.user
@@ -32,10 +33,15 @@ def index(request):
     else:
         return HttpResponseRedirect(reverse('myContent:index'))
 
-# methods to manage groups
+# html form for user adding a group
 def addGroupInterface(request):
-    context = {}
-    return render(request, 'Groups/addGroupInterface.html', context)
+    if request.user.is_authenticated():
+        context = {}
+        return render(request, 'Groups/addGroupInterface.html', context)
+    else:
+        return HttpResponseRedirect(reverse('myContent:index'))
+
+# user adds a group
 def addGroup(request):
     # check for user authentication, POST
     if request.method == 'POST' and request.user.is_authenticated():
@@ -43,7 +49,7 @@ def addGroup(request):
         password = request.POST.get("password")
         user = request.user
     else:
-        return HttpResponse("Attempted to make group without authentication or POST")
+        return HttpResponseRedirect(reverse('myContent:index'))
     # create group
     group = Group(name=name,password=password)
     group.save() # only need to save once
@@ -51,6 +57,8 @@ def addGroup(request):
     group.moderators.add(user)
     group.members.add(user)
     return HttpResponseRedirect(reverse('Groups:groupMemberView',args=[group.pk]))
+
+# user removes self from group
 def removeSelfFromGroup(request,group_pk):
     group = Group.objects.get(pk=group_pk)
     group.admins.remove(request.user)
@@ -58,21 +66,20 @@ def removeSelfFromGroup(request,group_pk):
     group.members.remove(request.user)
     return HttpResponseRedirect(reverse('myContent:index'))
 
-# only admins can delete group
+# delete a group. Only admins can delete group
 def deleteGroup(request,group_pk):
     group = Group.objects.get(pk=group_pk)
-    group.delete()
-    return HttpResponseRedirect(reverse('myContent:index'))
+    if group.admins.filter(pk=request.user.pk).exists() or request.user.is_superuser:
+        group.delete()
+        return HttpResponseRedirect(reverse('myContent:index'))
+    else:
+        return HttpResponseRedirect(reverse('myContent:index'))
 
 # anybody can join a group if they know the password
 def joinGroupInterface(request,group_pk):
     group = Group.objects.get(pk=group_pk)
-    if request.user.is_authenticated():
-        context = {'group':group,}
-        return render(request,'Groups/joinGroupInterface.html',context)
-    else:
-        context = {'group':group,}
-        return render(request,'Groups/joinGroupInterface.html',context)
+    context = {'group':group,}
+    return render(request,'Groups/joinGroupInterface.html',context)
 # anybody can join a group if they know the password
 def joinGroup(request,group_pk):
     name = request.POST.get("name")
@@ -86,16 +93,18 @@ def joinGroup(request,group_pk):
         context = {'group':group,'incorrect_password':True}
         return render(request,'Groups/joinGroupInterface.html',context)
 
-# only admin can change permission
+# Make a user an admin, moderator, or member. Only admin can change permission
 def changePermissions(request,user_pk,group_pk):
-    # check for user authentication, POST
+    # check for user authentication, POST, and make sure user is an admin of the group
     if request.method == 'POST' and request.user.is_authenticated():
         group = Group.objects.get(pk=group_pk)
         user = User.objects.get(pk=user_pk)
         permission = request.POST.get("permission")
     else:
-        return HttpResponse("No authentication or POST")
-    #
+        return HttpResponseRedirect(reverse('myContent:index'))
+    if not group.admins.filter(pk=request.user.pk).exists() and not request.user.is_superuser:
+        return HttpResponseRedirect(reverse('myContent:index'))
+    # Change permission
     if permission == "admin":
         group.members.add(user)
         group.moderators.add(user)
@@ -114,17 +123,20 @@ def changePermissions(request,user_pk,group_pk):
         group.admins.remove(user)
     return HttpResponseRedirect(reverse('Groups:groupAdminPanel',args=[group.pk]))
 
-# only admin can change group name
+# Change name of group. Only admin can change group name
 def changeGroupName(request,group_pk):
-    # check for user authentication, POST
+    # check for user authentication, POST, and make sure user is an admin of the group
     if request.method == 'POST' and request.user.is_authenticated():
         form_text = request.POST.get("form_text")
     else:
         return HttpResponse("Attempted to make group without authentication or POST")
+    if not group.admins.filter(pk=request.user.pk).exists() and not request.user.is_superuser:
+        return HttpResponseRedirect(reverse('myContent:index'))
     group = Group.objects.get(pk=group_pk)
     group.name = form_text
     group.save()
     return HttpResponseRedirect(reverse('Groups:groupAdminPanel',args=[group.pk]))
+
 # only admin can change group description
 def changeGroupDescription(request,group_pk):
     pass
@@ -135,6 +147,8 @@ def changeGroupPassword(request,group_pk):
         form_text = request.POST.get("form_text")
     else:
         return HttpResponse("Attempted to make group without authentication or POST")
+    if not group.admins.filter(pk=request.user.pk).exists() and not request.user.is_superuser:
+        return HttpResponseRedirect(reverse('myContent:index'))
     group = Group.objects.get(pk=group_pk)
     group.password = form_text
     group.save()
@@ -146,24 +160,32 @@ def changeAcceptingNewMembers(request,group_pk):
 def changeUploadApprovalRequired(request,group_pk):
     pass
 
-
-# only moderators and admins can see admin panel
+# only admins can see admin panel
 def groupAdminPanel(request,group_pk):
     group = Group.objects.get(pk=group_pk)
+    if not group.admins.filter(pk=request.user.pk).exists() and not request.user.is_superuser:
+        return HttpResponseRedirect(reverse('myContent:index'))
     admins = group.admins.all()
     moderators = group.moderators.exclude(id__in=admins)
     members = group.members.exclude(id__in=admins).exclude(id__in=moderators)
     context = {"group":group,"admins":admins,"moderators":moderators,"members":members}
     return render(request, 'Groups/groupAdminPanel.html', context)
-# only moderators and admins can see admin panel
+# only moderators and admins and original uploader can see moderator panel
 def documentAdminPanel(request,document_pk):
     document = Document.objects.get(pk=document_pk)
+    group = document.group
+    if not group.moderators.filter(pk=request.user.pk).exists() and not request.user.is_superuser and document.user.pk != request.user.pk:
+        return HttpResponseRedirect(reverse('myContent:index'))
     context = {"document":document,}
     return render(request, 'Groups/documentAdminPanel.html', context)
 
 # only group members can see group view
 def groupMemberView(request,group_pk):
+    # check to make sure user is member
     group = Group.objects.get(pk=group_pk)
+    if not group.members.filter(pk=request.user.pk).exists() and not request.user.is_superuser:
+        return HttpResponseRedirect(reverse('myContent:index'))
+    # return html
     isModerator = group.is_moderator(request.user)
     isAdmin = group.is_admin(request.user)
     documents = group.document_set.all()
@@ -171,14 +193,16 @@ def groupMemberView(request,group_pk):
     context = {'group':group,'isModerator':isModerator,'isAdmin':isAdmin,'documents':documents}
     return render(request, 'Groups/groupMemberView.html', context)
 
-# only moderators and admins can delete documents
+# only moderators and original uploader can delete documents
 def deleteDocument(request,document_pk):
-    doc = Document.objects.get(pk=document_pk)
-    url = reverse('Groups:groupMemberView',args=[doc.group.pk])
+    document = Document.objects.get(pk=document_pk)
+    group = document.group
+    if not group.moderators.filter(pk=request.user.pk).exists() and not request.user.is_superuser and document.user.pk != request.user.pk:
+        return HttpResponseRedirect(reverse('myContent:index'))
     file_path=settings.MEDIA_ROOT
-    os.remove(file_path+'/'+doc.docfile.name)
-    doc.delete()
-    return HttpResponseRedirect(url)
+    os.remove(file_path+'/'+document.docfile.name)
+    document.delete()
+    return HttpResponseRedirect(reverse('Groups:groupMemberView',args=[document.group.pk]))
 
 # all members should be able to send notifications
 def sendNotification(request,group_pk):
@@ -188,6 +212,8 @@ def sendNotification(request,group_pk):
         next_url = request.POST.get("next_url")
     else:
         return HttpResponse("Attempted to make group without authentication or POST")
+    if not group.members.filter(pk=request.user.pk).exists() and not request.user.is_superuser:
+        return HttpResponseRedirect(reverse('myContent:index'))
     # create group
     group = Group.objects.get(pk=group_pk)
     users = group.members.all()
