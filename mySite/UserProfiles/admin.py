ba01d89bf48653d67fc6f6a07e49d4670de5cc66
Michael Chiang <michaelchiang@Michaels-iMac.local>
2016-03-29 16:21:36 -0700

    Admin for userprofile changed (could not edit admin before)

--- mySite/UserProfiles/admin.py
+++ mySite/UserProfiles/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+
 from .models import UserProfile, Notification
-from django.contrib.auth.models import User
 
-admin.site.unregister(User)
-
-class UserProfileInline(admin.StackedInline):
-    model = UserProfile
-
-class UserProfileAdmin(UserAdmin):
-    inlines = [ UserProfileInline, ]
-
-admin.site.register(User, UserProfileAdmin)
+admin.site.register(UserProfile)
 admin.site.register(Notification)
